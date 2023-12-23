53526079ac345067effe0a2009dc8f0599811954
night-sornram <sornram.chalernma@gmail.com>
2023-12-23 22:08:52 +0700

    add

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,20 @@
 import type { Config } from 'tailwindcss'
 
+const {colors : defaultColors }  = require("tailwindcss/defaultTheme")
+
+const colors = {
+  ...defaultColors,
+  ...{
+    "custom" : {
+      "100" : "hsl(193, 38%, 86%)",
+      "200" : "hsl(150, 100%, 66%)",
+      "300" : "hsl(217, 19%, 38%)",
+      "400" : "hsl(217, 19%, 24%)",
+      "500" : "hsl(218, 23%, 16%)",
+    }
+  }
+}
+
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -13,6 +28,7 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors : colors,
     },
   },
   plugins: [],
